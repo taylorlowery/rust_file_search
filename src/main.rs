a64839dd46ea2b20f2ccb0816c36115403c6e267
Taylor Lowery <taylorlowery07@gmail.com>
2023-12-24 08:33:57 -0800

    move file search code to module

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
 use clap::Parser;
-use std::error::Error;
-use std::ffi::OsStr;
-use std::fs::{self};
 use std::path::PathBuf;
 // get command line args that should be a path
 // if that path exists,
@@ -22,40 +19,46 @@ pub struct CliArgs {
     filetype: Option<String>,
 }
 
-pub fn walk_files(
-    path: &PathBuf,
-    recursive: bool,
-    filetype: Option<String>,
-) -> Result<Vec<PathBuf>, Box<dyn Error>> {
-    if path.is_file() {
-        return Ok(vec![path.clone()]);
-    }
-    let mut matches: Vec<PathBuf> = Vec::new();
+mod file_search {
+    use std::error::Error;
+    use std::ffi::OsStr;
+    use std::fs::{self};
+    use std::path::PathBuf;
+    pub fn walk_files(
+        path: &PathBuf,
+        recursive: bool,
+        filetype: Option<String>,
+    ) -> Result<Vec<PathBuf>, Box<dyn Error>> {
+        if path.is_file() {
+            return Ok(vec![path.clone()]);
+        }
+        let mut matches: Vec<PathBuf> = Vec::new();
 
-    for entry in fs::read_dir(path)? {
-        let entry = entry?;
-        let path = entry.path();
-        match path.is_dir() {
-            true => {
-                // only search through sub-directories if recursive
-                if recursive == true {
-                    matches.append(&mut walk_files(&path, recursive, filetype.clone()).unwrap())
-                }
-            }
-            // if filetype is defined, only add the specified type
-            false => match &filetype {
-                None => matches.push(path),
-                Some(filetype) => {
-                    let filetype = OsStr::new(&filetype);
-                    if path.extension().unwrap() == filetype {
-                        matches.push(path)
+        for entry in fs::read_dir(path)? {
+            let entry = entry?;
+            let path = entry.path();
+            match path.is_dir() {
+                true => {
+                    // only search through sub-directories if recursive
+                    if recursive == true {
+                        matches.append(&mut walk_files(&path, recursive, filetype.clone()).unwrap())
                     }
                 }
-            },
+                // if filetype is defined, only add the specified type
+                false => match &filetype {
+                    None => matches.push(path),
+                    Some(filetype) => {
+                        let filetype = OsStr::new(&filetype);
+                        if path.extension().unwrap() == filetype {
+                            matches.push(path)
+                        }
+                    }
+                },
+            }
         }
-    }
 
-    Ok(matches)
+        Ok(matches)
+    }
 }
 
 fn main() {
@@ -66,7 +69,7 @@ fn main() {
         true => println!("Eureka!"),
         false => println!("Alas!"),
     }
-    for path in walk_files(&args.path, args.recursive, args.filetype).unwrap() {
+    for path in file_search::walk_files(&args.path, args.recursive, args.filetype).unwrap() {
         println!("{:?}", path);
     }
 }
